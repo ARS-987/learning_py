d82c723f64c5a1bcd2ce0e2d38a4a07e445ad52b
Habres <a_r_s333@yahoo.com>
2021-08-01 20:05:01 +0430

    9

---
+++ for2.py
@@ -0,0 +1,11 @@
+num = int(input('enter num: '))
+sum = 0
+for i in range(1,num+1):
+    sum += i
+print (sum)
+
+num1 = int(input('enter num1: '))
+res = 1
+for k in range(1,num1+1):
+    res *= k
+print (res)
