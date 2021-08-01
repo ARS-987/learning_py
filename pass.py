4e293ec18444b59ed9c84afab286a21d1b95cb2f
Habres <a_r_s333@yahoo.com>
2021-08-01 18:22:23 +0430

    7

---
+++ pass.py
@@ -0,0 +1,6 @@
+correct_pass = "090877ars"
+password = input('enter pass: ')
+if password == correct_pass:
+    print ('Ok')
+else:
+    print ('error!')
---
+++ shart2.py
@@ -0,0 +1,10 @@
+num = int(input('enter num: '))
+
+if num < 10 and num >=0:
+    print ('mardod')
+elif num >=10 and num <=20:
+    print ('gabol')
+elif num >20:
+    print ('mare than 20!')
+else:
+    print ('error!')
