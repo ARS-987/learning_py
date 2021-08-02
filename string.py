3d2f39693a8ca5c7b2ad74dec9b818032eadde2b
Habres <a_r_s333@yahoo.com>
2021-08-02 11:59:53 +0430

    14

---
+++ string.py
@@ -0,0 +1,18 @@
+str = 'alirahimi'
+#str= str[:3]+' m'+str[5:]
+print(str)
+
+if 'k' in str:
+    print('str has k')
+else:
+    print('str has not k')
+
+if str.isdigit():
+    print('is digit')
+else:
+    print('not digit')
+
+if str.isalpha():
+    print('is alpha')
+else:
+    print('not alpha')
