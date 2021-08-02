de2ce2e16a25f39a8b81942adf8428a86138ee86
Habres <a_r_s333@yahoo.com>
2021-08-02 19:25:56 +0430

    16

---
+++ string3.py
@@ -0,0 +1,10 @@
+str = 'ali maryam reza fatemeh'
+l = str.split(' ')
+print(l)
+print('/'.join(l))
+url = 'F:\\Libraries\\Pictures\\ARS\\Eagle'
+l = url.split('\\')
+print(l)
+l2 = '/'.join(l)
+print(l2)
+print('/'.join(url.split('\\')))
