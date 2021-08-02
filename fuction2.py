9f955de9d1e0ce30158a0ff0e36ea89d3f6d8e31
Habres <a_r_s333@yahoo.com>
2021-08-02 20:14:43 +0430

    18.2

--- fuction2.py
+++ fuction2.py
@@ -3,19 +3,19 @@ def zarb(x):
 
 zarb(2)
 
-def zarb(x,y):
+def zarb1(x,y):
     return x * y
-res = zarb(3, 3)
+res = zarb1(3, 3)
 print(res)
 
-def zarb(x,y):
+def zarb2(x,y):
     return (x*y , x*2 , y*3)
-res2 = zarb(5, 7)
+res2 = zarb2(5, 7)
 print(res2)
 
-def zarb(x,y):
+def zarb3(x,y):
     return (x*y , x*2 , y*3)
-res2,x2,y3 = zarb(5, 7)
+res2,x2,y3 = zarb3(5, 7)
 print(res2)
 print(x2)
 print(y3)
