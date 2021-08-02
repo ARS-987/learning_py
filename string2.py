2c227818ef92ffce130e40a3237179476f69ce76
Habres <a_r_s333@yahoo.com>
2021-08-02 19:03:07 +0430

    15

---
+++ string2.py
@@ -0,0 +1,12 @@
+str = input('enter full name: ')
+res = ''
+for i in str:
+    if i.isalpha():
+        if i.isupper():
+            res += i.lower()
+        else:
+            res += i.upper()
+    else:
+        res += i
+print(str)
+print(res)
