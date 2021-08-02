5c2d45729e071a2ac99597a1d33664dfb3badd62
Habres <a_r_s333@yahoo.com>
2021-08-02 11:34:18 +0430

    13

---
+++ tuple.py
@@ -0,0 +1,15 @@
+t = (1,2,3,4,5,6,7,8,9,0)
+print(type(t))
+print(t[3])
+print(t[3:7])
+print(max(t))
+print(min(t))
+l = ['a','b','c']
+print(type(l))
+t1 = tuple(l)
+print(type(t1))
+t2 = t1 +t
+print(t2)
+l1 = list(t2)
+print(type(l1),
+l1)
