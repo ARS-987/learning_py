ae45a37eaaad4d832f804ee13416025989a40e9b
Habres <a_r_s333@yahoo.com>
2021-08-04 12:53:18 +0430

    22

---
+++ module/mod1.py
@@ -0,0 +1,13 @@
+def plus3():
+    num = int(input())
+    res = num + 3
+    print(res)
+
+plus3()
+
+def zarb3():
+    num = int(input())
+    res = num * 3
+    print(res)
+
+zarb3()
---
+++ module/proj.py
@@ -0,0 +1,5 @@
+import mod1
+print('finish')
+
+from mod1 import zarb3
+zarb3()
