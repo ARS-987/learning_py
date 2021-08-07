1a032777ba78ea7b518e1bf6f18273b604d18716
ars-987 <a_r_s333@yahoo.com>
2021-08-07 09:19:15 +0430

    27

---
+++ oop.py
@@ -0,0 +1,16 @@
+class person():
+
+    name = 'ali'
+    age = 23
+    vazn = 70
+
+    def sey_hello(self):
+        print('salam')
+
+obj1 = person()
+obj2 = person()
+
+obj1.name = 'mahdi'
+
+print(obj1.name)
+print(obj2.name)
